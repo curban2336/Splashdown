826ec621045c6cae1d1593f511045c08c358ca02
DaltonM827 <153662451+DaltonM827@users.noreply.github.com>
2026-03-13 22:41:22 -0400

    Mine Scripts added
    Added scripts for mines including individual scripts for each mine and a manager script

---
+++ Assets/Scripts/MineOneScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MineOneScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //transform.position = new Vector3(-500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MineSpawn.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MineSpawn : MonoBehaviour
+{
+    private Vector2 MineHeight;
+    private int RollHeight;
+    private int MineNum = 1;
+    [SerializeField] GameObject MineOne;
+    [SerializeField] GameObject MineTwo;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
